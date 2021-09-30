c2c39915a2de5081aeaa2118eadf258a97d1d390
ngwatso <ngwatson79@yahoo.com>
2021-09-30 17:58:33 -0400

    cleaned up getPosts

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import matter from 'gray-matter';
 import Layout from '@/components/Layout';
 import Post from '@/components/Post';
-import { sortByDate } from '@/utils/index';
+import { getPosts } from '@/lib/posts';
 
 export default function CategoryBlogPage({ posts, categoryName }) {
 	return (
@@ -55,21 +55,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { category_name } }) {
 	const files = fs.readdirSync(path.join('posts'));
 
-	const posts = files.map((filename) => {
-		const slug = filename.replace('.md', '');
-
-		const markdownWithMeta = fs.readFileSync(
-			path.join('posts', filename),
-			'utf-8'
-		);
-
-		const { data: frontmatter } = matter(markdownWithMeta);
-
-		return {
-			slug,
-			frontmatter,
-		};
-	});
+	const posts = getPosts();
 
 	// Filter posts by category
 	const categoryPosts = posts.filter(
@@ -80,7 +66,7 @@ export async function getStaticProps({ params: { category_name } }) {
 
 	return {
 		props: {
-			posts: categoryPosts.sort(sortByDate),
+			posts: categoryPosts,
 			categoryName: category_name,
 		},
 	};
